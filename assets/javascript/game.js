284b6bd469b71a78730b81761c3addc32bffc066
bethgranade <bgranado488@gmail.com>
2019-10-18 23:38:44 -0500

    added images for crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["./assets/images/Crystal1.png", "./assets/images/Crystal2.png", "./assets/images/Crystal3.png", "./assets/images/Crystal4.png"];
