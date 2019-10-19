e7abf597000a25e8b9f9fc8bcca0a2ce2380fb45
bethgranade <bgranado488@gmail.com>
2019-10-19 10:45:30 -0500

    added win/loss css and fixed html to show count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,3 +18,42 @@ function resetCrystals () {
         $(".crystal-images").append(crystal);
     }
 }
+
+function resetHTML () {
+    $(".target-number").html(targetNumber);
+    $(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+    $(".score-number").html(counter);
+    $(".crystal-images").empty();
+}
+
+function totalReset () {
+    randomTargetNumber ();
+    counter = 0;
+    resetHTML ();
+    resetCrystals ();
+}
+
+// Running Code
+
+// Inital Page Set Up
+randomTargetNumber();
+resetHTML ();
+resetCrystals ();
+
+// Click Functions
+function crystalClick () {
+    //attr returns first value of selected html element
+    counter += parseInt($(this).attr("value"));
+    $(".score-number").html(counter);
+    if (counter == targetNumber) {
+        wins++;
+        totalReset();
+    }
+    else if (counter > targetNumber) {
+        losses++;
+        totalReset();
+    };
+};
+
+//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute crystalClick function
+$(document).on("click", ".crystal", crystalClick);
