89fbd1dae21efe601184b71e0cac385ffa53580b
bethgranade <bgranado488@gmail.com>
2019-10-18 23:49:13 -0500

    add images to html for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,18 @@ var wins = 0;
 var losses = 0;
 var counter = 0;
 var images = ["./assets/images/Crystal1.png", "./assets/images/Crystal2.png", "./assets/images/Crystal3.png", "./assets/images/Crystal4.png"];
+
+function randomTargetNumber () {
+    targetNumber = Math.floor(Math.random() * 102) + 19;
+}
+
+function resetCrystals () {
+    for (var i = 0; i < images.length; i++) {
+        var crystal = $("<img>");
+        crystal.addClass("crystal");
+        crystal.attr("src", images[i]);
+        crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+        crystal.attr("height", "100");
+        $(".crystal-images").append(crystal);
+    }
+}
